09ada01e05b0644ace81b2298839a58a8262211b
mittalsarthak <mittalsarthak24@gmail.com>
2020-11-10 14:17:22 +0530

    used @Autowired and @Qualifier for dependency injection

--- src/com/example/demo/autowiring/Human.java
+++ src/com/example/demo/autowiring/Human.java
@@ -1,8 +1,23 @@
 package com.example.demo.autowiring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class Human {
     private Heart heart;
 
+    public Human (){
+        System.out.println("Default Constructor is being called");
+    }
+
+
+    public Human(Heart heart) {
+        this.heart = heart;
+        System.out.println("Parameterized Constructor is being called");
+    }
+
+    @Autowired
+    @Qualifier("humanHeart")
     public void setHeart(Heart heart) {
         this.heart = heart;
     }
@@ -10,6 +25,8 @@ public class Human {
     public void startPumping() {
         if(heart != null) {
             heart.pump();
+            System.out.println("Name of the animal is:" + heart.getNameOfAnimal());
+            System.out.println("Number of hearts present are:" + heart.getNoOfHearts());
         }
         else{
             System.out.println("You are dead..");
